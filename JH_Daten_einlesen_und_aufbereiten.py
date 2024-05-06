7fe97d2307634cf30db85bd7076fd39101a3f991
julhardt <juliane.hardt@uni-wh.de>
2024-05-06 22:47:15 +0200

    JH5 commit staged changes

--- JH_Daten_einlesen_und_aufbereiten.py
+++ JH_Daten_einlesen_und_aufbereiten.py
@@ -6,12 +6,20 @@ from pathlib import Path
 import numpy as np
 #import pyreadstat
 
-#os.getcwd()
+data_iris = pd.read_csv('data/iris.csv',sep=',',header=0)
+data_iris.head()
 
+data_census = pd.read_csv('data/census.csv',sep=',',header=0)
+data_census.head()
+
+
+
+#os.getcwd()
 
 # data = pd.read_csv('..\data/iris.csv',sep=',',header=0)
 #data=pd.read_excel("C:/Users/jhardt/Documents/PYTHON/Data/Census-Datensatz.xlsx")
-data = open(r"C:\Users\jhardt\Documents\PYTHON\Data")
+# data = open(r"C:\Users\jhardt\Documents\PYTHON\Data\Census-Datensatz.xlsx")
+# Pfad klappt, aber Permission Error (evtl. wg. fehlendem Admin-Account)
 #Census-Datensatz.xlsx
 
 #data_iris = pd.read_csv('data/iris.csv',sep=',',header=0)
