9bd026fd0edbde0cf3d62340429eb2d2e449f3d9
julhardt <juliane.hardt@uni-wh.de>
2024-05-06 22:25:16 +0200

    JH3 add pathlib to requirements

--- JH_Daten_einlesen_und_aufbereiten.py
+++ JH_Daten_einlesen_und_aufbereiten.py
@@ -2,7 +2,24 @@
 
 import pandas as pd
 import os
+from pathlib import Path
 import numpy as np
+#import pyreadstat
 
-data_iris = pd.read_csv('data/iris.csv',sep=',',header=0)
-data_iris.head()
+p = Path.cwd()
+print(p)
+# /Users/mbp/Documents/my-project/python-snippets/notebook
+
+print(type(p))
+
+
+os.getcwd()
+
+
+# data = pd.read_csv('..\data/iris.csv',sep=',',header=0)
+#data=pd.read_excel("C:/Users/jhardt/Documents/PYTHON/Data/Census-Datensatz.xlsx")
+
+
+
+#data_iris = pd.read_csv('data/iris.csv',sep=',',header=0)
+#data_iris.head()
