0404793ce3146fd3a726b41aab17724e19c7bdce
julhardt <juliane.hardt@uni-wh.de>
2024-05-06 22:34:50 +0200

    JH4 try data open

--- JH_Daten_einlesen_und_aufbereiten.py
+++ JH_Daten_einlesen_und_aufbereiten.py
@@ -6,20 +6,20 @@ from pathlib import Path
 import numpy as np
 #import pyreadstat
 
-p = Path.cwd()
-print(p)
-# /Users/mbp/Documents/my-project/python-snippets/notebook
-
-print(type(p))
-
-
-os.getcwd()
+#os.getcwd()
 
 
 # data = pd.read_csv('..\data/iris.csv',sep=',',header=0)
 #data=pd.read_excel("C:/Users/jhardt/Documents/PYTHON/Data/Census-Datensatz.xlsx")
-
-
+data = open(r"C:\Users\jhardt\Documents\PYTHON\Data")
+#Census-Datensatz.xlsx
 
 #data_iris = pd.read_csv('data/iris.csv',sep=',',header=0)
 #data_iris.head()
+
+# Befehle für Modul pathlib
+# p = Path.cwd()
+# print(p)
+# /Users/mbp/Documents/my-project/python-snippets/notebook
+
+# print(type(p))
